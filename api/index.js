f7a94153f54a466675fd527b246aee6fb08cb7c2
David Munro <munrodj86@gmail.com>
2023-08-20 20:44:12 -0400

    fixed routes

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
-app.get('api', (req, res) => {
-  res.sendFile(__dirname + '/app.html');
-});
+// app.get('api', (req, res) => {
+//   res.sendFile(__dirname + '/app.html');
+// });
 
 app.get('/api/ping', (req, res) => {
   res.send("pong");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
 
 module.exports = app;
