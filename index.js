90633ae72ed253e6ec7202817d340b3e2661f4b1
David Munro <munrodj86@gmail.com>
2023-08-20 21:20:29 -0400

    replit

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
